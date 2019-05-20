ccef6c42af07b32ba7ff09027559339d64ca8981
Gregory Wehmeier <wehmenstein@gmail.com>
2019-05-20 16:04:46 -0500

    replace repo with next.js

---
+++ .next/static/webpack/static/development/pages/index.js.01638d1ba7729ff75c0d.hot-update.js
@@ -0,0 +1,60 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./node_modules/react/index.js":
+false,
+
+/***/ "./node_modules/react/index.js?5712":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_afa7b25a60452594c240 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_afa7b25a60452594c240 */ "dll-reference dll_afa7b25a60452594c240"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js?5712");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/gregoryweh/Desktop/g-dev/mrYakamoto.github.io/pages/index.js";
+
+
+function Home() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "greggw");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ }),
+
+/***/ "dll-reference dll_6dc2816e14fab51b8269":
+false,
+
+/***/ "dll-reference dll_afa7b25a60452594c240":
+/*!*******************************************!*\
+  !*** external "dll_afa7b25a60452594c240" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_afa7b25a60452594c240;
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.01638d1ba7729ff75c0d.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/index.js.6c422c93642ba5c1b580.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/gregoryweh/Desktop/g-dev/mrYakamoto.github.io/pages/index.js";
+
+
+function Home() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, " greggw ");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.6c422c93642ba5c1b580.hot-update.js.map
